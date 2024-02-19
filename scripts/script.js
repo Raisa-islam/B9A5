b691b8b752de3f0182bfdd06626d581d3b29d0f1
Raisa-islam <raisa.isl683@gmail.com>
2024-02-19 08:21:33 +0600

    toggle cell done

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
-let seatCount = 0;
-const gridCells = document.querySelectorAll('#gridContainer ');
+
+let t = 0;
+let m = 0;
+let total_free_seat=40;
+
+document.querySelectorAll('.grid-cell').forEach(cell => {
+    cell.addEventListener('click', () => selectCell(cell));
+    cell.addEventListener('mouseover', () => highlightCell(cell));
+    cell.addEventListener('mouseout', () => unhighlightCell(cell));
+});
 
 function goToTicket() {
     //console.log("btn clicked!");
@@ -9,7 +17,27 @@ function goToTicket() {
     }
 }
 
-const cells = document.querySelectorAll('.grid-cell');
-cells.forEach(cell => {
-    cell.addEventListener('click', () => selectCell(cell.id));
-});
+
+function selectCell(cell) {
+    // Check if the cell is selected or not
+    if (cell.classList.contains('selected')) {
+        // Decrease the value of t
+        t--;
+        m = 0;
+        // Remove the 'selected' class
+        cell.classList.remove('selected');
+        console.log(t);
+    } else {
+        // Increase the value of t
+        t++;
+        // Add the 'selected' class
+        cell.classList.add('selected');
+        console.log(t);
+        m=1;
+    }
+
+    // Update the background color based on the value of t
+    cell.style.backgroundColor = m === 1 ? 'blue' : '#F7F8F8';
+
+}
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -2,7 +2,17 @@ function toggleCellCol(event){
     event.target.classList.toggle('bg-blue-500');
     event.target.classList.toggle('text-white');
 }
+function highlightCell(cell) {
+    if (!cell.classList.contains('selected')) {
+        cell.style.backgroundColor = 'green';
+    }
+}
 
+function unhighlightCell(cell) {
+    if (!cell.classList.contains('selected')) {
+        cell.style.backgroundColor = '#F7F8F8';
+    }
+}
 function addSeat(){
 
 }
